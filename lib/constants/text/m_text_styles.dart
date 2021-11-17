dae04e72178f82bead25a3a73f44f9f660e3cadf
medibuddy-edward <edward-kwak@medibuddygroup.com>
2021-11-17 22:27:15 +0900

    fix: m_text_styles.dart 재수정.

--- lib/constants/text/m_text_styles.dart
+++ lib/constants/text/m_text_styles.dart
@@ -5,214 +5,214 @@ class MTextStyles {
     10: const TextStyle(
         fontWeight: FontWeight.w400,
         fontSize: 10,
-        // height: 1.8,
+        height: 1.8,
         letterSpacing: 12 * 0.02),
     12: const TextStyle(
         fontWeight: FontWeight.w400,
         fontSize: 12,
-        // height: 1.8,
+        height: 1.8,
         letterSpacing: 12 * 0.01),
     14: const TextStyle(
         fontWeight: FontWeight.w400,
         fontSize: 14,
-        // height: 1.74,
+        height: 1.74,
         letterSpacing: 14 * 0.01),
     16: const TextStyle(
         fontWeight: FontWeight.w400,
-        // height: 1.72,
+        height: 1.72,
         fontSize: 16,),
     18: const TextStyle(
         fontWeight: FontWeight.w400,
         fontSize: 18,
-        // height: 1.68,
+        height: 1.68,
         letterSpacing: 18 * -0.01),
     20: const TextStyle(
         fontWeight: FontWeight.w400,
         fontSize: 20,
-        // height: 1.68,
+        height: 1.68,
         letterSpacing: 21 * -0.01),
     22: const TextStyle(
         fontWeight: FontWeight.w400,
         fontSize: 22,
-        // height: 1.64,
+        height: 1.64,
         letterSpacing: 24 * -0.01),
     24: const TextStyle(
         fontWeight: FontWeight.w400,
         fontSize: 24,
-        // height: 1.60,
+        height: 1.60,
         letterSpacing: 26 * -0.01),
     26: const TextStyle(
         fontWeight: FontWeight.w400,
         fontSize: 26,
-        // height: 1.60,
+        height: 1.60,
         letterSpacing: 28 * -0.01),
     28: const TextStyle(
         fontWeight: FontWeight.w400,
         fontSize: 28,
-        // height: 1.58,
+        height: 1.58,
         letterSpacing: 32 * -0.01),
   };
   static const Map<int, TextStyle> medium = {
     10: const TextStyle(
         fontWeight: FontWeight.w500,
         fontSize: 10,
-        // height: 1.8,
+        height: 1.8,
         letterSpacing: 12 * 0.02),
     12: const TextStyle(
         fontWeight: FontWeight.w500,
         fontSize: 12,
-        // height: 1.8,
+        height: 1.8,
         letterSpacing: 12 * 0.01),
     14: const TextStyle(
         fontWeight: FontWeight.w500,
         fontSize: 14,
-        // height: 1.74,
+        height: 1.74,
         letterSpacing: 14 * 0.01),
     16: const TextStyle(
         fontWeight: FontWeight.w500,
-        // height: 1.72,
+        height: 1.72,
         fontSize: 16,),
     18: const TextStyle(
         fontWeight: FontWeight.w500,
         fontSize: 18,
-        // height: 1.68,
+        height: 1.68,
         letterSpacing: 18 * -0.01),
     20: const TextStyle(
         fontWeight: FontWeight.w500,
         fontSize: 20,
-        // height: 1.68,
+        height: 1.68,
         letterSpacing: 21 * -0.01),
     22: const TextStyle(
         fontWeight: FontWeight.w500,
         fontSize: 22,
-        // height: 1.64,
+        height: 1.64,
         letterSpacing: 24 * -0.01),
     24: const TextStyle(
         fontWeight: FontWeight.w500,
         fontSize: 24,
-        // height: 1.60,
+        height: 1.60,
         letterSpacing: 26 * -0.01),
     26: const TextStyle(
         fontWeight: FontWeight.w500,
         fontSize: 26,
-        // height: 1.60,
+        height: 1.60,
         letterSpacing: 28 * -0.01),
     28: const TextStyle(
         fontWeight: FontWeight.w500,
         fontSize: 28,
-        // height: 1.58,
+        height: 1.58,
         letterSpacing: 32 * -0.01),
   };
   static const Map<int, TextStyle> bold = {
     12: const TextStyle(
         fontWeight: FontWeight.w700,
         fontSize: 12,
-        // height: 1.8,
+        height: 1.8,
         letterSpacing: 12 * 0.01),
     14: const TextStyle(
         fontWeight: FontWeight.w700,
         fontSize: 14,
-        // height: 1.74,
+        height: 1.74,
         letterSpacing: 14 * 0.01),
     16: const TextStyle(
         fontWeight: FontWeight.w700,
-        // height: 1.72,
+        height: 1.72,
         fontSize: 16,),
     18: const TextStyle(
         fontWeight: FontWeight.w700,
         fontSize: 18,
-        // height: 1.68,
+        height: 1.68,
         letterSpacing: 18 * -0.01),
     21: const TextStyle(
         fontWeight: FontWeight.w700,
         fontSize: 21,
-        // height: 1.64,
+        height: 1.64,
         letterSpacing: 21 * -0.01),
     24: const TextStyle(
         fontWeight: FontWeight.w700,
         fontSize: 24,
-        // height: 1.60,
+        height: 1.60,
         letterSpacing: 24 * -0.01),
     26: const TextStyle(
         fontWeight: FontWeight.w700,
         fontSize: 26,
-        // height: 1.60,
+        height: 1.60,
         letterSpacing: 26 * -0.01),
     28: const TextStyle(
         fontWeight: FontWeight.w700,
         fontSize: 28,
-        // height: 1.58,
+        height: 1.58,
         letterSpacing: 28 * -0.01),
     32: const TextStyle(
         fontWeight: FontWeight.w700,
         fontSize: 32,
-        // height: 1.55,
+        height: 1.55,
         letterSpacing: 32 * -0.02),
     36: const TextStyle(
         fontWeight: FontWeight.w700,
         fontSize: 36,
-        // height: 1.50,
+        height: 1.50,
         letterSpacing: 36 * -0.02),
     48: const TextStyle(
         fontWeight: FontWeight.w700,
         fontSize: 48,
-        // height: 1.50,
+        height: 1.50,
         letterSpacing: 48 * -0.03),
   };
   static const Map<int, TextStyle> black = {
     12: const TextStyle(
         fontWeight: FontWeight.w900,
         fontSize: 12,
-        // height: 1.8,
+        height: 1.8,
         letterSpacing: 12 * 0.01),
     14: const TextStyle(
         fontWeight: FontWeight.w900,
         fontSize: 14,
-        // height: 1.74,
+        height: 1.74,
         letterSpacing: 14 * 0.01),
     16: const TextStyle(
         fontWeight: FontWeight.w900,
-        // height: 1.72,
+        height: 1.72,
         fontSize: 16,),
     18: const TextStyle(
         fontWeight: FontWeight.w900,
         fontSize: 18,
-        // height: 1.68,
+        height: 1.68,
         letterSpacing: 18 * -0.01),
     21: const TextStyle(
         fontWeight: FontWeight.w900,
         fontSize: 21,
-        // height: 1.64,
+        height: 1.64,
         letterSpacing: 21 * -0.01),
     24: const TextStyle(
         fontWeight: FontWeight.w900,
         fontSize: 24,
-        // height: 1.60,
+        height: 1.60,
         letterSpacing: 24 * -0.01),
     26: const TextStyle(
         fontWeight: FontWeight.w900,
         fontSize: 26,
-        // height: 1.60,
+        height: 1.60,
         letterSpacing: 26 * -0.01),
     28: const TextStyle(
         fontWeight: FontWeight.w900,
         fontSize: 28,
-        // height: 1.58,
+        height: 1.58,
         letterSpacing: 28 * -0.01),
     32: const TextStyle(
         fontWeight: FontWeight.w900,
         fontSize: 32,
-        // height: 1.55,
+        height: 1.55,
         letterSpacing: 32 * -0.02),
     36: const TextStyle(
         fontWeight: FontWeight.w900,
         fontSize: 36,
-        // height: 1.50,
+        height: 1.50,
         letterSpacing: 36 * -0.02),
     48: const TextStyle(
         fontWeight: FontWeight.w900,
         fontSize: 48,
-        // height: 1.50,
+        height: 1.50,
         letterSpacing: 48 * -0.03),
   };
 }
