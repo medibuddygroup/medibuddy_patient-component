95a6b08d1027a124276e3b550d448ffebc4c99e2
medibuddy-edward <edward-kwak@medibuddygroup.com>
2022-09-05 13:36:25 +0900

    [Fix] License Updated, m_text_colors.dart 의 오타 수정, started working on atomic design pattern.

--- lib/constants/text/m_text_colors.dart
+++ lib/constants/text/m_text_colors.dart
@@ -4,6 +4,6 @@ import 'package:medibuddy_ui_kit/medibuddy_ui_kit.dart';
 class MTextColors {
   static final Color primary = MColors.gray[900]!;
   static final Color secondary = MColors.gray[600]!;
-  static final Color teriary = MColors.gray[400]!;
+  static final Color tertiary = MColors.gray[400]!;
   static final Color placeholderWithBold = MColors.gray[100]!;
 }
