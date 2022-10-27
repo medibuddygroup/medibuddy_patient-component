90982e4fc9aba9b0a90726cce71580fbdcabd147
medibuddy-edward <edward-kwak@medibuddygroup.com>
2022-10-27 10:42:10 +0900

    [Add] date time converter util added. made by Paul.

---
+++ lib/util/date_time_converter_util.dart
@@ -0,0 +1,27 @@
+class DateTimeConverterUtil {
+
+  /// This function was created by Paul.
+  String convertDateTimeToMinutesAgo(DateTime start, DateTime end) {
+    Duration difference = start.difference(end);
+    int differenceDay = difference.inDays;
+    int differenceHours = difference.inHours;
+    int differenceMinutes = difference.inMinutes;
+
+    if(differenceDay >= 3) {
+      if (start.year == end.year)
+        return '${end.month}월 ${end.day}일';
+      else
+        return '${end.year}년 ${end.month}월 ${end.day}일';
+    }
+
+    if(differenceDay >= 1) return '$differenceDay일 전';
+
+    if(differenceMinutes > 59) return '$differenceHours시간 전';
+
+    if(differenceMinutes >= 3)
+      return '$differenceMinutes분 전';
+    else
+      return "방금 전";
+  }
+
+}
